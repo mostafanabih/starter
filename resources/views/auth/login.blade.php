6d950ab359495043b3ebef78e4b6fd5b4c1ba668
mostafa kasem <mostafanabihkasem@gmail.com>
2021-08-15 14:49:47 +0200

    me

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">email</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">email Or mobile</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required  autofocus>
+                                <input id="identify" type="text" class="form-control @error('identify') is-invalid @enderror" name="identify" value="{{ old('identify') }}" required >
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
